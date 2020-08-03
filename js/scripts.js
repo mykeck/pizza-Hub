1a1237b161d0d4ba097ac417d138fd1b76d25260
MikeC <kiragumike@gmail.com>
2020-08-03 11:50:24 +0300

    updated javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,34 @@
-function myFunction() {
-    var checkBox = document.getElementById("deliveries");
-    var text = document.getElementById("address");
-  
-    // If the checkbox is checked, display the output text
-    if (checkBox.checked == true){
-      text.style.display = "block";
-    } else {
-      text.style.display = "none";
+
+  function myFunction(name,location,number){
+    var name=document.getElementById("name").value
+    var location=document.getElementById('location').value
+    var number=document.getElementById("number").value
+    event.preventDefault();
+    if(name===""||location===""||number===""){
+        alert("Fields can't be empty");
     }
-  }
+    else{
+        alert(" Hello " + name + " we've received your order it will be processed and deliverd to " +location)
+    }
+    
+    };
+
+    function myPrice(){
+        var crust=document.getElementById("crust").value
+        var topping=document.getElementById("topping").value
+        var size=document.getElementById("size").value
+        var quantity=document.getElementById("quantity").value
+        var final=(crust + topping +size ) *quantity;
+     
+     document.getElementById("1").innerHTML= " we received your order of pizza size  " + size  ;
+     document.getElementById("2").innerHTML= " with topping of  "+ topping ;
+     document.getElementById("3").innerHTML= " And crust of  "+ crust;
+     document.getElementById("4").innerHTML= " And you selected " + quantity +" of them ";
+     };
+
+     $(document).click(function(){
+        $(".btn").click(function(){
+            $("#summary").toggle();
+            $("#down").toggle();
+        })
+    })
