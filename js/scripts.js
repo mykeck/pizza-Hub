eb4aae45e76efa33b70812b096044cb14009c88d
MikeC <kiragumike@gmail.com>
2020-08-04 07:47:57 +0300

    added price

--- js/scripts.js
+++ js/scripts.js
@@ -21,23 +21,54 @@
         var quantity=document.getElementById("quantity").value
         var final=(crust + topping +size ) *quantity;
      
-     document.getElementById("1").innerHTML= " we received your order of pizza size  " + size  ;
+     document.getElementById("1").innerHTML= " we received your order of pizza size  " + size + "flavor of" +type ;
      document.getElementById("2").innerHTML= " with topping of  "+ topping ;
      document.getElementById("3").innerHTML= " And crust of  "+ crust;
      document.getElementById("4").innerHTML= " And you selected " + quantity +" of them ";
      document.getElementById("5").innerHTML= " Total price " + price +" / ";
 
      };
-     switch (pizzasize) {
-         case "large":
-             var price=1200;
-              break;
-         case "medium"
-         var price
+     switch (pizzasize){
+        case "large":
+         var price=1200;
+        break;
+        case "medium":
+          price=1000;
+        break;
+        case "regular":
+          price=600;
+        break;
+        default:
+          alert("select your pizza size");
+      }
+      switch(pizzacrust){
+        case "cripsy":
+         var crust_price=200;
+        break;
+        case "stuffed":
+          crust_price=150;
+          break;
+        case "glutenfree":
+          crust_price=300;
+        break;
+        default:
+          alert("select your crust");
+      }
+      switch(pizzatopping){
+        case "mushrooms":
+         var topping_cost=200;
+        break;
+        case "onions":
+          topping_cost=100
+        break;
+        case "extracheese":
+          topping_cost=250;
+        break;
+        default:
+          alert("select your topping");
+      }
+      var total=(price+crust_price+topping_cost)*pizzaquantity;
      
-         default:
-             break;
-     }
 
      $(document).ready(function(){
         $(".btn").click(function(){
